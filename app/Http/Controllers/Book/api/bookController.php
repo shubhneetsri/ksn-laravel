17fa09ba1562de26b5b898786a7de1f6bfee8510
shubhneetsri <shubhneet.srivastava@gmail.com>
2020-04-03 10:53:25 +0530

    changes

--- app/Http/Controllers/Book/api/bookController.php
+++ app/Http/Controllers/Book/api/bookController.php
@@ -7,16 +7,18 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
 
 class bookController extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    function add(){
+    function add(Request $request){
+        
         $returnArr['param'] = "test";
         $returnArr['error'] = "The Line number is : ". __line__;
         $returnArr['status'] = "The directory is : ". __dir__;
 
-        return response()->json($returnArr);
+        return response()->json($request);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/add-book', 'Book\api\bookController@add')->middleware('cors');
+Route::post('/add-book', 'Book\api\bookController@add')->middleware('cors');
